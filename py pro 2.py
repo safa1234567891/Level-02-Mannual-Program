fee4197f0f928d9554605066b1f309af67cab3d3
SAFA THASNIM <safathasnim269@gmail.com>
2025-07-30 15:44:56 +0530

    S2

---
+++ py pro 2.py
@@ -0,0 +1,8 @@
+print("Circle program")
+print(" _ _ _ _ _ _ _ _ _ _ _")
+r=int(input("Enter the r value:"))
+print("RESULT")
+pc=2*22/7*r
+print("Perimater os circle:",pc)
+ac=22/7*r*r
+print("Area of circle:",ac)
---
+++ py pro 3.py
@@ -0,0 +1,11 @@
+print("Triangle program")
+print(" _ _ _ _ _ _ _ _ _ _ _ _")
+a=int(input("Enter the a value:"))
+b=int(input("Enter the b value:"))
+c=int(input("Enter the c value:"))
+h=int(input("Enter the h value:"))
+print("RESULT")
+pt=a+b+c
+print("Perimeter of triangle:",pt)
+at=1/2*b*h
+print("Area of triangle:",at)

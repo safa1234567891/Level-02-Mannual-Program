93937355bd499bc0875da6dbee18677777ef3ee4
SAFA THASNIM <safathasnim269@gmail.com>
2025-07-30 15:53:36 +0530

    S2

---
+++ py pro 1.py
@@ -0,0 +1,14 @@
+print("Square and Rectangle")
+print(" _ _ _ _ _ _ _ _ _ _ _ _ ")
+a=int(input("Enter the a value:"))
+b=int(input("Enter the b value:"))
+l=int(input("Enter the b value:"))
+print("RESULT")
+ps=4*a
+print("perimeter of square:",ps)
+ass=a*a
+print("Area of square:",ass)
+pr=2*(l+b)
+print("Perimeter of rectangle:",pr)
+ar=l*b
+print("Area of rectangle:",ar)

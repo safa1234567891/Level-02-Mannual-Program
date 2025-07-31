3d7de2733b54acf483636bf2e8eca576aeec3ffe
SAFA THASNIM <safathasnim269@gmail.com>
2025-07-31 14:21:11 +0530

    S2

---
+++ py pro 4.py
@@ -0,0 +1,9 @@
+print("SUM OF N NUMBERS ")
+print("-------------------")
+n=int(input("Enter a n value:"))
+sum=n*(n+1)/2
+print("Sum of n values:",sum)
+sums=n*(n+1)*(2*n+1)/6
+print("Sum of n square values:",sums)
+sumc=(n*(n+1)/2)**2
+print("Sum of n cube values:",sumc)
---
+++ py pro 5.py
@@ -0,0 +1,14 @@
+print("Exponential program")
+print("--------------------")
+x=int(input("Enter x value:"))
+y=int(input("Enter y value:"))
+z=int(input("Enter z value:"))
+print("RESULT")
+p=x*x+y*y+z*z
+print("P=",p)
+q=x*x*x+y*y+z+20
+print("Q=",q)
+r=x*x*y+y*y*z+z*z*x+10
+print("R=",r)
+s=x*x*x*y*y*z+(x*y*z)+x*y*z
+print("S=",s)
